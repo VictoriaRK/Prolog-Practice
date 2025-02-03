06c48d155387197c913fa1562eda302c30dd36d5
VictoriaRK <vkrushovska@gmail.com>
2025-02-03 11:35:00 +0000

    Update sublists.pl

--- sublists.pl
+++ sublists.pl
@@ -3,3 +3,12 @@ sublista(A, [_|L]) :-
     sublista(A, L).
 sublista([], _).
 
+sublistb(B, A) :-
+    append(_, L, A), 
+    append(B, _, L).  
+
+append([], [], []).
+append(A, [], A).
+append([], A, A).
+append([S], L, [S | L]).
+append([H | T], L, [H | R]) :- append(T, L, R).
